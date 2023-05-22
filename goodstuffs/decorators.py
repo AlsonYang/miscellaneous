31f0bc4a2346f385a830c1538c7bb310d96dc190
AlsonYang <31328407+AlsonYang@users.noreply.github.com>
2023-05-23 09:41:41 +1000

    Update decorators.py
    add template

--- goodstuffs/decorators.py
+++ goodstuffs/decorators.py
@@ -1,30 +1,54 @@
 """
-- Decorator without extra arguments
-- Decorator with extra arguments
+- 1. Decorator without extra parameters
+- 2. Decorator with extra parameters
 - @wraps to carry forward the base function information. 
     Without it, when you call help(mysleep), it will show the information for wrapper function instead of the base function 
 """
 
+import time
 from functools import wraps
 from datetime import datetime
-import time
 
-## 1
+## 0: decorator template
+def decorator(f):
+    """ 
+    f: The function to be decorated
+    wraps(f): carry thru the function signiture into `wrapper`
+    `*args, **kwargs`: pass along any arguments to the decorated function
+
+    If without `<do anything>`, the decorated version will be the same as the 
+        original version. 
+    """
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        # <do anything>
+        ret = f(*args, **kwargs) # execute the decorated function as it is and stored the result
+        # <do anything>
+        return ret
+    return wrapper
+
+
+## 1: decorator without extra parameters
 def timeit(f):
     """decorator with no extra arguments"""
     @wraps(f)
     def wrapper(*args, **kwargs):
         t1 = datetime.now()
-        f(*args, **kwargs)
+        ret = f(*args, **kwargs)
         print(datetime.now() - t1)
+        return ret
     return wrapper
+
 @timeit
 def mysleep(seconds=1):
     time.sleep(seconds)
+    return "im awake now"
 # sleep = timeit(sleep, 1, 2)
-mysleep(1)
 
-## 2
+print(mysleep(1))
+
+
+## 2: decorator with extra parameters
 class timeit_with_msg:
     """ decorator with extra argument `msg`"""
     def __init__(self, msg='Hello'):
@@ -35,14 +59,17 @@ class timeit_with_msg:
         def wrapper(*args, **kwargs):
             print(self._msg)
             t1 = datetime.now()
-            f(*args, **kwargs)
+            ret = f(*args, **kwargs)
             print(datetime.now() - t1)
+            return ret
         return wrapper
 
-@timeit_with_msg(msg='Hi')
+@timeit_with_msg(msg='I am going to sleep now')
 def mysleep(seconds=1):
     """sleep for n seconds"""
     time.sleep(seconds)
+    return "im awake now"
 # sleep = timeit_with_msg(msg='Hi')(sleep)
-mysleep(1)
+
+print(mysleep(1))
 help(mysleep)
